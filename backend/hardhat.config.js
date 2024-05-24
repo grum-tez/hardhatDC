23d7fb3910d600404661e9924034231c5c0860c6
grum-tez <grum.tez@gmail.com>
2024-05-24 21:36:14 +0000

    moved to using dotenv for handling backend secrets

---
+++ backend/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    etherlinkTestnet: {
+      url: "https://node.ghostnet.etherlink.com",
+      accounts: [process.env.ETHERLINK_TESTNET_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      etherlinkTestnet: process.env.ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "etherlinkTestnet",
+        chainId: 128123,
+        urls: {
+          apiURL: "https://testnet-explorer.etherlink.com/api",
+          browserURL: "https://testnet-explorer.etherlink.com"
+        }
+      },
+    ]
+  },
+};
