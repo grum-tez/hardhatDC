e7c57da12d482ed321e6c1e87c5a0e499a6860c5
grum-tez <116943950+grum-tez@users.noreply.github.com>
2024-05-28 17:24:50 +0000

    aider: Update BattleModule to generate a new random wallet, add it to the local Hardhat node, and deploy the contract using it.
    # Aider chat conversation:
    
    USER: Is it really not possible to generate a new random local address and its key and use that?
    
    ASSISTANT: Yes, it is possible to generate a new random local address and its key and use that. However, you need to ensure that the local node recognizes this new account and has the private key for it. This can be done by adding the generated account to the local Hardhat node.
    
    Let's update the `BattleModule.js` to generate a new random wallet, add it to the local Hardhat node, and then use it to deploy the contract.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    backend/ignition/modules/BattleModule.js
    ```javascript
    <<<<<<< SEARCH
    const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
    const { ethers } = require("ethers");
    const { network } = require("hardhat");
    
    console.log('BattleModule is being executed.');
    
    const battleModule = buildModule("BattleModule", (m) => {
      const DEFAULT_BATTLE_MASTER_CHAMPION_ID = 6;
    
      // Define the parameter for the battle master champion ID with a default value
      const battleMasterChampionId = m.getParameter("battleMasterChampionId", DEFAULT_BATTLE_MASTER_CHAMPION_ID);
    
      // Use the first account provided by the local Hardhat node
      const [deployer] = await network.provider.send("eth_accounts");
    
      // Deploy the BattleContract with the specified battle master champion ID from the deployer's address
      const battleContract = m.contract("BattleContract1", [battleMasterChampionId], { from: deployer });
    
      // Return the contract as a part of the module
      return { battleContract };
    });
    
    module.exports = battleModule;
    =======
    const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
    const { ethers } = require("ethers");
    const { network } = require("hardhat");
    
    console.log('BattleModule is being executed.');
    
    const battleModule = buildModule("BattleModule", async (m) => {
      const DEFAULT_BATTLE_MASTER_CHAMPION_ID = 6;
    
      // Define the parameter for the battle master champion ID with a default value
      const battleMasterChampionId = m.getParameter("battleMasterChampionId", DEFAULT_BATTLE_MASTER_CHAMPION_ID);
    
      // Generate a random wallet
      const randomWallet = ethers.Wallet.createRandom();
    
      // Add the random wallet to the local Hardhat node
      await network.provider.send("hardhat_setBalance", [
        randomWallet.address,
        "0x1000000000000000000", // 1 ETH in hex
      ]);
    
      // Deploy the BattleContract with the specified battle master champion ID from the random wallet's address
      const battleContract = m.contract("BattleContract1", [battleMasterChampionId], { from: randomWallet.address });
    
      // Return the contract as a part of the module
      return { battleContract };
    });
    
    module.exports = battleModule;
    >>>>>>> REPLACE
    ```
    
    This will generate a new random wallet, add it to the local Hardhat node with a balance of 1 ETH, and then use it to deploy the contract.

--- backend/ignition/modules/BattleModule.js
+++ backend/ignition/modules/BattleModule.js
@@ -4,17 +4,23 @@ const { network } = require("hardhat");
 
 console.log('BattleModule is being executed.');
 
-const battleModule = buildModule("BattleModule", (m) => {
+const battleModule = buildModule("BattleModule", async (m) => {
   const DEFAULT_BATTLE_MASTER_CHAMPION_ID = 6;
 
   // Define the parameter for the battle master champion ID with a default value
   const battleMasterChampionId = m.getParameter("battleMasterChampionId", DEFAULT_BATTLE_MASTER_CHAMPION_ID);
 
-  // Use the first account provided by the local Hardhat node
-  const [deployer] = await network.provider.send("eth_accounts");
+  // Generate a random wallet
+  const randomWallet = ethers.Wallet.createRandom();
 
-  // Deploy the BattleContract with the specified battle master champion ID from the deployer's address
-  const battleContract = m.contract("BattleContract1", [battleMasterChampionId], { from: deployer });
+  // Add the random wallet to the local Hardhat node
+  await network.provider.send("hardhat_setBalance", [
+    randomWallet.address,
+    "0x1000000000000000000", // 1 ETH in hex
+  ]);
+
+  // Deploy the BattleContract with the specified battle master champion ID from the random wallet's address
+  const battleContract = m.contract("BattleContract1", [battleMasterChampionId], { from: randomWallet.address });
 
   // Return the contract as a part of the module
   return { battleContract };
