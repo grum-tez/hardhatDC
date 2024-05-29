189a18094dc89e1fc967eaf6a6613ae4ec98a269
grum-tez <116943950+grum-tez@users.noreply.github.com>
2024-05-29 04:06:07 +0000

    (no commit message provided)

--- backend/ignition/modules/BattleModule.js
+++ backend/ignition/modules/BattleModule.js
@@ -1,26 +1,15 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-const { ethers } = require("ethers");
-const { network } = require("hardhat");
 
 console.log('BattleModule is being executed.');
 
-const DEFAULT_BATTLE_MASTER_CHAMPION_ID = 6;
-
-// Generate a random wallet
-const randomWallet = ethers.Wallet.createRandom();
-
-// Add the random wallet to the local Hardhat node
-await network.provider.send("hardhat_setBalance", [
-  randomWallet.address,
-  "0x1000000000000000000", // 1 ETH in hex
-]);
-
 const battleModule = buildModule("BattleModule", (m) => {
+  const DEFAULT_BATTLE_MASTER_CHAMPION_ID = 6;
+
   // Define the parameter for the battle master champion ID with a default value
   const battleMasterChampionId = m.getParameter("battleMasterChampionId", DEFAULT_BATTLE_MASTER_CHAMPION_ID);
 
-  // Deploy the BattleContract with the specified battle master champion ID from the random wallet's address
-  const battleContract = m.contract("BattleContract1", [battleMasterChampionId], { from: randomWallet.address });
+  // Deploy the BattleContract with the specified battle master champion ID
+  const battleContract = m.contract("BattleContract1", [battleMasterChampionId]);
 
   // Return the contract as a part of the module
   return { battleContract };
